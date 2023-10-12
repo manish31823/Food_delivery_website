fd6cb8928410ae1babbaa1a4fdd0680bb2663ec1
manish31823 <128781985+manish31823@users.noreply.github.com>
2023-10-12 20:59:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const menuBtn = document.querySelector(".mobileMenu")
+const mobileNavigation = document.querySelector(".mobileNavigation")
+console.log(menuBtn)
+
+let openNavigation = true
+menuBtn.addEventListener("click",()=>{
+    if(openNavigation){
+        mobileNavigation.classList.add("active")
+        openNavigation = false
+    }else{
+
+        mobileNavigation.classList.remove("active")
+        openNavigation = true
+    }
+
+})
